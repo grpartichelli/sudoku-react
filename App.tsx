fac17003d318b781dce440a41d7f6cb5640e0790
Gabriel RIzzolli Partichelli <gabrielpartichelli@gmail.com>
2020-07-20 18:55:20 -0300

    Added icons

--- App.tsx
+++ App.tsx
@@ -2,18 +2,26 @@ import React from 'react';
 import { StyleSheet, Text, View, StatusBar } from 'react-native';
 import { COLORS } from './constants/colors';
 import RoundButton from './RoundButton';
+import FontAwesomeIcon from "react-native-vector-icons/FontAwesome";
+import IoniconIcon from "react-native-vector-icons/Ionicons";
 export default function App() {
   return (
     <View style={styles.container}>
       <StatusBar barStyle="light-content" />
       <View style={styles.topContainer}>
         <Text style={styles.text}>Watermelon Sudoku</Text>
+
       </View >
       <View style={styles.middleContainer}>
+        <FontAwesomeIcon name="cog" size={150} style={styles.icon} />
         <RoundButton />
         <RoundButton />
       </View>
-      <View style={styles.bottomContainer}></View>
+      <View style={styles.bottomContainer}>
+        <FontAwesomeIcon name="cog" size={45} style={styles.icon} />
+        <IoniconIcon name="md-menu-outline" size={45} style={styles.icon} />
+        <IoniconIcon name='md-color-palette' size={45} style={styles.icon} />
+      </View>
     </View >
   );
 }
@@ -22,30 +30,38 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: COLORS.darkBackground,
-    alignItems: 'center',
+    alignItems: 'stretch',
     justifyContent: 'center',
   },
   topContainer: {
-    flex: 2,
+    flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+
   }
   ,
   middleContainer: {
     flex: 2,
-    alignItems: 'center',
+    alignItems: 'stretch',
     justifyContent: 'center',
+
+
   }
   ,
   bottomContainer: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'flex-start',
+    justifyContent: 'space-evenly',
+    flexDirection: 'row',
+
   },
   text: {
     color: COLORS.lightPink,
     fontSize: 30,
 
   },
+  icon: {
+    color: COLORS.lightPink,
+  }
 
 });
--- RoundButton.tsx
+++ RoundButton.tsx
@@ -25,6 +25,7 @@ const styles = StyleSheet.create({
         borderRadius: 20,
         backgroundColor: COLORS.lightPink,
         margin: 10,
+        alignItems: 'center'
     },
     text: {
         color: COLORS.backgroundDark,
