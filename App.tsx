994bfbef49e246db03fc84b6db78e3d5870fa414
Gabriel RIzzolli Partichelli <gabrielpartichelli@gmail.com>
2020-07-22 18:35:03 -0300

    Added watermelon icon

--- App.tsx
+++ App.tsx
@@ -2,8 +2,12 @@ import React from 'react';
 import { StyleSheet, Text, View, StatusBar } from 'react-native';
 import { COLORS } from './constants/colors';
 import RoundButton from './RoundButton';
+
+//Some icons
 import FontAwesomeIcon from "react-native-vector-icons/FontAwesome";
 import IoniconIcon from "react-native-vector-icons/Ionicons";
+import WatermelonIcon from './assets/WatermelonIcon';
+
 export default function App() {
   return (
     <View style={styles.container}>
@@ -12,15 +16,19 @@ export default function App() {
         <Text style={styles.text}>Watermelon Sudoku</Text>
 
       </View >
-      <View style={styles.middleContainer}>
-        <FontAwesomeIcon name="cog" size={150} style={styles.icon} />
+
+      <View style={styles.middleTopContainer}>
+        <WatermelonIcon fill={COLORS.lightPink} style={styles.watermelonIcon} />
+      </View>
+
+      <View style={styles.middleBottomContainer}>
         <RoundButton />
         <RoundButton />
       </View>
       <View style={styles.bottomContainer}>
-        <FontAwesomeIcon name="cog" size={45} style={styles.icon} />
-        <IoniconIcon name="md-menu-outline" size={45} style={styles.icon} />
-        <IoniconIcon name='md-color-palette' size={45} style={styles.icon} />
+        <FontAwesomeIcon name="cog" size={40} style={styles.bottomIcon} />
+        <IoniconIcon name="md-menu" size={40} style={styles.bottomIcon} />
+        <IoniconIcon name='md-color-palette' size={40} style={styles.bottomIcon} />
       </View>
     </View >
   );
@@ -32,22 +40,29 @@ const styles = StyleSheet.create({
     backgroundColor: COLORS.darkBackground,
     alignItems: 'stretch',
     justifyContent: 'center',
-  },
+  },//Title container
   topContainer: {
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
 
   }
-  ,
-  middleContainer: {
-    flex: 2,
+  ,//WatermelonIcon container
+  middleTopContainer: {
+    flex: 1,
     alignItems: 'stretch',
     justifyContent: 'center',
 
 
   }
-  ,
+  ,//New game and score buttons containers
+  middleBottomContainer: {
+    flex: 2,
+    alignItems: 'stretch',
+    justifyContent: 'center',
+
+
+  },//Icons containers
   bottomContainer: {
     flex: 1,
     alignItems: 'center',
@@ -60,8 +75,12 @@ const styles = StyleSheet.create({
     fontSize: 30,
 
   },
-  icon: {
+  bottomIcon: {
     color: COLORS.lightPink,
+  },
+  watermelonIcon: {
+
+    flex: 1
   }
 
 });
