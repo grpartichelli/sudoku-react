2c230fdb9739b4eb3fb5a46904eeed86400e2449
Gabriel Rizzolli Partichelli <gabrielpartichelli@gmail.com>
2020-08-03 20:39:28 -0300

    add a few buttons

--- containers/Sudoku.tsx
+++ containers/Sudoku.tsx
@@ -1,25 +1,101 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Text, View, StyleSheet, TouchableOpacity } from 'react-native';
 import { COLORS } from "../constants/colors"
 
-
-const renderBottomButtons = () => {
-
-  for (let i = 0; i < 10; i++) {
+class BottomButton extends Component {
+  render() {
     return (
-
-
       <TouchableOpacity
-        style={styles.bottomButton}
+        style={bottomStyles.bottomButton}
         activeOpacity={.5}
         onPress={() => console.log("BottomButton pressed.")}
       >
-        <Text style={styles.bottomButtonText}> 1 </Text>
+        <Text style={bottomStyles.bottomButtonText}> 1 </Text>
       </TouchableOpacity>
     );
   }
+}
+
+const renderBottomButtons = () => {
+
+
+  return (
+    <View style={bottomStyles.container}>
+      <View style={bottomStyles.topContainer}>
+        <View style={bottomStyles.bottomButtonContainer} >
+          <BottomButton />
+          <BottomButton />
+          <BottomButton />
+          <BottomButton />
+        </View>
+        <View style={bottomStyles.bottomButtonContainer}>
+          <BottomButton />
+          <BottomButton />
+          <BottomButton />
+          <BottomButton />
+        </View>
+      </View>
+      <View style={bottomStyles.bottomContainer}>
+        <Text style={styles.text}>More Icons Here</Text>
+      </View>
+    </View>
+
+
+  );
+
 
 }
+
+const bottomStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: COLORS.background,
+    alignItems: 'stretch',
+    justifyContent: 'center',
+  },
+  topContainer: {
+
+    flex: 2,
+    alignItems: 'stretch',
+    justifyContent: 'center',
+
+
+
+  },
+  bottomContainer: {
+
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'row',
+
+
+  },
+  bottomButtonContainer: {
+
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'row',
+
+
+
+  },
+
+  bottomButtonText: {
+    color: COLORS.mainColor,
+    fontSize: 25
+  },
+  bottomButton: {
+    padding: 5,
+    marginHorizontal: 2.5,
+    borderWidth: 2,
+    borderRadius: 100,
+    borderColor: COLORS.mainColor,
+
+  },
+})
+
 
 export default function Sudoku() {
   return (
@@ -56,7 +132,7 @@ const styles = StyleSheet.create({
   },
 
   middleContainer: {
-    flex: 5,
+    flex: 4.5,
     alignItems: 'center',
     justifyContent: 'center',
     borderWidth: 3,
@@ -65,20 +141,10 @@ const styles = StyleSheet.create({
   },
   bottomContainer: {
     flex: 2,
-    alignItems: 'center',
+    alignItems: 'stretch',
     justifyContent: 'center',
 
   },
 
-  bottomButtonText: {
-    color: COLORS.mainColor,
-    fontSize: 24
-  },
-  bottomButton: {
-    padding: 10,
-    borderRadius: 50,
-    borderWidth: 1,
-    alignItems: 'center',
-    borderColor: COLORS.mainColor
-  },
+
 })//Title container
